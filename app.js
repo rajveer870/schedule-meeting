286ab143917e289b3209339c100de3b8a333fc44
anshu <rajveeranshu786@gmail.com>
2021-11-02 20:56:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+module.exports={
+    x:12,
+    y:09,
+    z:function(){
+    
+        return 10;
+    }
+    }
---
+++ index.js
@@ -0,0 +1,28 @@
+/*const app =require('./app')
+a=10;
+
+b=70;
+console.log(a+b);
+console.log(app.z())
+const arr =[1,2,3,44,55,66,77,88];
+arr.filter((item)=>{
+    console.log(item)
+})
+let result=arr.filter((item)=>{
+    return item==3
+})
+console.log(result)
+let result1=arr.filter((item)=>{
+return item>3
+})
+console.warn(result1)*/
+//const fs =require('fs');
+//fs.writeFileSync("hello.txt","like and subscribe")
+
+//for find a filename
+
+console.log("->>",__filename)
+//const fs=require('fs').writeFileSync;
+//fs("abc.txt","there is a new way to create file")
+const color=require('colors');
+console.log("package.json".red);
